5754f3e4b426a82761c6ddea0bd760d181bf61fc
gulnar-hqvrd <gulnarhagverdiyeva18@gmail.com>
2025-12-17 12:53:01 +0400

    addboxjs search

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-let count = 0;
+let headerInput = document.getElementById("header");
+let contentInput = document.getElementById("content");
+let addButton = document.getElementById("add");
+let searchInput = document.getElementById("search");
 
-document.getElementById("add").addEventListener("click", function () {
-  count++;
-  createBox(count);
+addButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  let headerValue = headerInput.value;
+  let contentValue = contentInput.value;
+  if (headerValue && contentValue) {
+    let box = document.createElement("div");
+    box.className = "box";
+    let header = document.createElement("h2");
+    header.textContent = headerValue;
+    let content = document.createElement("p");
+    content.textContent = contentValue;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Sil";
+    deleteBtn.className = "delete";
+
+     deleteBtn.addEventListener("click", function () {
+      box.remove();
+      console.log("salam");
+      
+    });
+
+    box.appendChild(header);
+    box.appendChild(content);
+    box.appendChild(deleteBtn);
+    document.getElementById("container").appendChild(box);
+    headerInput.value = "";
+    contentInput.value = "";
+  }
+
+  console.log("Salam");
 });
 
-function createBox(num) {
-  const container = document.getElementById("container");
 
-  const box = document.createElement("div");
-  box.className = "box";
-  box.dataset.id = num;
+searchInput.addEventListener("keyup", function () {
+  let searchValue = searchInput.value.toLowerCase();
+  let boxes = document.querySelectorAll(".box");
 
-  box.innerHTML = `
-        <h1>Metn ${num}</h1>
-        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsa iste sit labore beatae atque cum rerum ipsam quaerat dolore.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsa iste sit labore beatae atque cum rerum ipsam quaerat dolore.${num}</p>
-        <button class="delete">Sil</button>
-    `;
+  boxes.forEach(function (box) {
+    let headerText = box.querySelector("h2").textContent.toLowerCase();
+    let contentText = box.querySelector("p").textContent.toLowerCase();
 
-  box.querySelector(".delete").addEventListener("click", function () {
-    box.remove();
+    if (
+      headerText.includes(searchValue) ||
+      contentText.includes(searchValue)
+    ) {
+      box.style.display = "block";
+    } else {
+      box.style.display = "none";
+    }
   });
-
-  container.appendChild(box);
-}
+});
